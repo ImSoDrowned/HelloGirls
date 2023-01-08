3930eb1c87101a58a7d15c9c346d7cf5ac61bbea
ImSoDrowned <86160973+ImSoDrowned@users.noreply.github.com>
2023-01-08 12:19:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-import os, json, random, threading, requests, time, instagrapi, traceback, os, pymongo, subprocess
-from tabnanny import check
+import json
+import requests
+import time
+import instagrapi
 from instagrapi import Client
 from bs4 import BeautifulSoup
-
-class glob:
-	shutup=False
-
-#__PROGRAMID__ = "c2JedU7LcPNfkW8H9yPVAFjTuPxeJh9"
-#__VERSION__ = "1.1"
 
 config = json.loads(open("./config.json", "r", encoding="utf-8").read())
 
@@ -17,9 +13,7 @@ class color:
     YELLOW = "\033[93m"
     RED = "\033[91m"
     RESET_ALL = "\033[0m"
-    BLUE = '\033[96m'
-
-
+    BLUE = "\033[96m"
 
 
 def user_handler():
@@ -32,66 +26,55 @@ def user_handler():
     return tokens
 
 
-def send_dm(username, password, userdm, messageText, count, i):
+def send_dm(username, password, messageText):
     try:
         username = username
         password = password
-        
-        while glob.shutup:
-        	time.sleep(5)
-        glob.shutup=True
-        
+
         cl = Client()
-        
-        #print('1')
+
         cl.login(username, password)
-        #print(2)
-        userid = cl.user_id_from_username(userdm)
-        print(userid)
-        for x in range(0,count):
+
+        for i, userdm in enumerate(user_handler()):
+            userid = cl.user_id_from_username(userdm)
+            i += 1
             a = cl.direct_send(messageText, [int(userid)])
-            print(f"{color.GREEN}[{i}] {color.RESET_ALL}Message succesfully sent to {color.GREEN}@{userdm} {color.RESET_ALL}with {color.GREEN}@{username}{color.RESET_ALL}.")
-            glob.shutup=False
+            print(
+                f"{color.GREEN}[{i}] {color.RESET_ALL}Message succesfully sent to {color.GREEN}@{userdm} {color.RESET_ALL}with {color.GREEN}@{username}{color.RESET_ALL}."
+            )
+
     except Exception as err:
-        print(f"{color.RED}[{i}]{color.RESET_ALL} An error occured when sending message to {color.RED}@{userdm} {color.RESET_ALL}account. Passing. ERROR: {err}")
-        #traceback.print_exc()
-        glob.shutup=False
-        pass
+        print(
+            f"{color.RED}[{i}]{color.RESET_ALL} An error occured when sending message to {color.RED}@{userdm} {color.RESET_ALL}account. Passing. ERROR: {err}"
+        )
+        # traceback.print_exc()
 
 
-def start(thread,msg):
-
-    try:
-        tx = []
-
-                    
-        for id, user_name in enumerate(user_handler()):
-                       
-            if threading.active_count() <= thread:
-                mT = threading.Thread(target=send_dm, args=(config["instagram_settings"]["username"],config["instagram_settings"]["password"], user_name, msg,config["script_settings"]["message_amount"],id+1))
-                #send_dm(config["instagram_settings"]["username"],config["instagram_settings"]["password"], random.choice(working_proxies), user_name, config["instagram_settings"]["text_message"])
-                mT.daemon = True
-                mT.start()
-                tx.append(mT)      
-        
-        for t in tx:
-            t.join(75)
-    except Exception as e:
-        traceback.print_exc()
-        pass
-
+def start_msg(msg):
+	with open("./usernames.txt", encoding="utf-8") as f:
+		lines = f.readlines()
+		prt = []
+		i = 1
+		for line in lines:
+		           if i <= 5:
+		           	prt.append(line.split("\n")[0])
+		           	i+=1
+		           else:
+		              prt.append("(and " + str(len(lines) - 5) + " more)")
+		              break
+		              i+=1
+		print(f"{color.BLUE}{len(lines)}{color.RESET_ALL}","Usernames Detected",f"{color.BLUE}[", ", ".join(prt), f"]{color.RESET_ALL}")
+		input("....")
+		send_dm(
+            config["instagram_settings"]["username"],
+            config["instagram_settings"]["password"],
+            msg,
+        )
 
 if __name__ == "__main__":
-    msg='Hello alwin'
-    
-    os.system("cls" if os.name == "nt" else "clear")
-    check = [True]
-    if check[0]: 
-        '''  
-        print(f"{color.GREEN}[+] License correct program starting... {color.RESET_ALL}")
-        time.sleep(2)
-        os.system("cls" if os.name == "nt" else "clear")'''
-        print(fr"""{color.GREEN}
+
+    print(
+        rf"""{color.GREEN}
 $$\   $$\           $$\ $$\                  $$$$$$\  $$\           $$\           
 $$ |  $$ |          $$ |$$ |                $$  __$$\ \__|          $$ |          
 $$ |  $$ | $$$$$$\  $$ |$$ | $$$$$$\        $$ /  \__|$$\  $$$$$$\  $$ | $$$$$$$\ 
@@ -102,15 +85,7 @@ $$ |  $$ |\$$$$$$$\ $$ |$$ |\$$$$$$  |      \$$$$$$  |$$ |$$ |      $$ |$$$$$$$ 
 \__|  \__| \_______|\__|\__| \______/        \______/ \__|\__|      \__|\_______/
 
     Press any key to start the script...
-    {color.RESET_ALL}    """)
-        input("....")
-
-        try:
-            start(config["script_settings"]["threading"],msg)
-            input("....")
-        except:
-            traceback.print_exc()
-            input(".....")
-    else:
-        print(f"{color.RED}[-] {check[1]} {color.RESET_ALL}")
-        input("....")
+    {color.RESET_ALL}    """
+    )
+    start_msg("Sup")
+    
